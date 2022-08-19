b4c12aa8cc901e446413a8a6f5cda5f4f9767d7a
alex <username.alex64@gmail.com>
2022-08-19 17:58:17 +0300

    Hibernate: retrieving Entity object from DB

--- src/main/java/hibernate/Test2.java
+++ src/main/java/hibernate/Test2.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().configure().addAnnotatedClass(Employee.class).buildSessionFactory();
 
@@ -16,6 +16,17 @@ public class Test1 {
             session.beginTransaction();
             session.save(emp);
             session.getTransaction().commit();
+
+            int myId = emp.getId();
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Employee employee = session.get(Employee.class, myId);
+            session.getTransaction().commit();
+
+            System.out.println(employee);
+
             System.out.println("done");
         } finally {
             factory.close();
